e4e09c8e6726eec62db84544db574c87e0b0c8d8
KalinduBihan <kalindubihanfdo@gmail.com>
2026-05-01 10:58:05 +0530

    commit 2

--- src/main/java/com/example/demo/service/AddressService.java
+++ src/main/java/com/example/demo/service/AddressService.java
@@ -1,5 +1,28 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.CityDTO;
+import com.example.demo.dto.CountryDTO;
+import com.example.demo.repository.AddressRepository;
+import com.example.demo.repository.CityRepository;
+import com.example.demo.repository.CountryRepository;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AddressService {
     
+    @Autowired
+    private AddressRepository addressRepository;
+    
+    @Autowired
+    private CustomerRepository customerRepository;
+    
+    @Autowired
+    private CityRepository cityRepository;
+    
+    @Autowired
+    private CountryRepository countryRepository;
+    
+    // Add service methods here
 }
